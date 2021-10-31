98070e02558a95bd95b2419453382ec512a8ee0f
rjachuthan <riturajachuthan@gmail.com>
2021-10-31 12:52:19 +0530

    Restart polybar and wallpaper

--- onlyHDMIdisplay.sh
+++ onlyHDMIdisplay.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 1920x1080 --pos 1366x0 --rotate normal
+. ~/.config/polybar/launch.sh
+wal -R
--- onlyLaptopDisplay.sh
+++ onlyLaptopDisplay.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 xrandr --output eDP-1 --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-1 --off
+. ~/.config/polybar/launch.sh
+wal -R
